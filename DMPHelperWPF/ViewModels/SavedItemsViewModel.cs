749911e6397d1a1e4535a0a96822bf52f0d69230
Ben Hall <bentomhall@gmail.com>
2018-04-11 21:00:07 -0400

    Working on saved item model

--- DMPHelperWPF/ViewModels/SavedItemsViewModel.cs
+++ DMPHelperWPF/ViewModels/SavedItemsViewModel.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibGenerator;
 
 namespace DMPHelperWPF.ViewModels
 {
-    class SavedItemsViewModel
+    public class SavedItemsViewModel : NotifyChangedBase
     {
+        private ObservableCollection<PersonViewModel> savedNPCModels = new ObservableCollection<PersonViewModel>();
+        private ObservableCollection<SettlementViewModel> savedSettlementModels = new ObservableCollection<SettlementViewModel>();
+        private ObservableCollection<DungeonViewModel> savedDungeonModels = new ObservableCollection<DungeonViewModel>();
+        private StorageHelper storage;
+
+        public SavedItemsViewModel(StorageHelper s)
+        {
+            storage = s;
+        }
+
+        public ObservableCollection<PersonViewModel> SavedNPCs { get => savedNPCModels; set => SetProperty(ref savedNPCModels, value); }
+        public ObservableCollection<SettlementViewModel> SavedSettlements { get => savedSettlementModels; set => SetProperty(ref savedSettlementModels, value); }
+        public ObservableCollection<DungeonViewModel> SavedDungeons { get => savedDungeonModels; set => SetProperty(ref savedDungeonModels, value); }
+
+        public void AddViewModel(NotifyChangedBase vm, Export.ExportTypes type)
+        {
+            switch (type)
+            {
+                case Export.ExportTypes.Dungeon:
+                    SavedDungeons.Add(vm as DungeonViewModel);
+                    return;
+                case Export.ExportTypes.Person:
+                    SavedNPCs.Add(vm as PersonViewModel);
+                    return;
+                case Export.ExportTypes.Settlement:
+                    SavedSettlements.Add(vm as SettlementViewModel);
+                    return;
+            }
+        }
     }
 }
